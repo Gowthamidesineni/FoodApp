1cf6209274e4d6dcd6f0cca94a3aee3e05d0ec34
Desineni Gowthami <desineni.gowthami@thoughtworks.com>
2023-11-02 12:13:05 +0530

    refactor

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React, { StrictMode } from 'react'
+import { createRoot } from "react-dom/client";
 
 import App from './App.jsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />,
-)
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
